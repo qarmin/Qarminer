33d4ca632353605828752380c6d75ebb7a6a0d48
Rafał Mikrut <mikrutrafal@protonmail.com>
2021-02-14 20:45:20 +0100

    Complete basics saving of data

--- CreateTestProject.gd
+++ CreateTestProject.gd
@@ -10,7 +10,6 @@ class ClassData:
 var classes : Array 
 
 func collect_data() -> void:
-	var debug_print : bool = true
 	var use_parent_methods : bool = false # Allows Node2D use Node methods etc. - it is a little slow option
 	
 	for name_of_class in Autoload.get_list_of_available_classes():
@@ -50,27 +49,68 @@ func remove_files_recursivelly(to_delete : String) -> void:
 	
 	assert(directory.open(to_delete) == OK)
 	directory.list_dir_begin()
-	var file_name = directory.get_next()
+	var file_name : String = directory.get_next()
 	while file_name != "":
 		if file_name != "." && file_name != "..":
 			if directory.current_is_dir():
 				file_name = to_delete + file_name + "/"
 				remove_files_recursivelly(file_name)
+			else:
+				file_name = to_delete + file_name
+				assert(file_name.find("/./") == -1 && file_name.begins_with("res://") && file_name.begins_with(base_dir) && file_name.find("//",6) == -1)
+#				print(file_name)
+				directory.remove(file_name)
 			
-			print(file_name)
-	#		directory.remove(file_name) # TODO, Test This
+			assert(file_name.find("/./") == -1 && file_name.begins_with("res://") && file_name.begins_with(base_dir) && file_name.find("//",6) == -1)
 		file_name = directory.get_next()
+
+#	print(to_delete)
+	assert(to_delete.find("/./") == -1 && to_delete.begins_with("res://") && to_delete.begins_with(base_dir) && to_delete.find("//",6) == -1)
+	directory.remove(to_delete) # TODO, Test This
 
 func create_basic_files() -> void:
 	var directory : Directory = Directory.new()
 	assert(directory.make_dir_recursive(base_dir + "2D/") == OK)
 	assert(directory.make_dir_recursive(base_dir + "3D/") == OK)
-	assert(directory.make_dir_recursive(base_dir + "Nodes/") == OK)
+	assert(directory.make_dir_recursive(base_dir + "Node/") == OK)
 	assert(directory.make_dir_recursive(base_dir + "Other/") == OK)
 	assert(directory.make_dir_recursive(base_dir + "Control/") == OK)
 	assert(directory.make_dir_recursive(base_dir + "Resource/") == OK)
 	assert(directory.make_dir_recursive(base_dir + "Reference/") == OK)
 
+	var file : File = File.new()
+	
+	
+	for class_data in classes:
+		var data_to_save : String  = ""
+		var file_name : String = base_dir
+		
+		if ClassDB.is_parent_class(class_data.name,"Spatial"): # TODO Fix in Godot 4.0
+			file_name += "3D"
+		elif ClassDB.is_parent_class(class_data.name,"Node2D"):
+			file_name += "2D"
+		elif ClassDB.is_parent_class(class_data.name,"Control"):
+			file_name += "Control"
+		elif ClassDB.is_parent_class(class_data.name,"Node"):
+			file_name += "Node"
+		elif ClassDB.is_parent_class(class_data.name,"Resource"):
+			file_name += "Resource"
+		elif ClassDB.is_parent_class(class_data.name,"Reference"):
+			file_name += "Reference"
+		else:
+			file_name += "Other"
+		
+		file_name += "/" + class_data.name
+		
+		data_to_save += "extends Node\n\n"
+		data_to_save += "var object : {} = {}.new()\n\n".replace("{}",class_data.name)
+		data_to_save += "func _ready():\n"
+		for i in range(class_data.function_names.size()):
+			data_to_save += "\tif randi() % 2 == 0:\n"
+			data_to_save += "\t\tobject." + class_data.function_names[i] + "(" + str(class_data.arguments[i]) + ")\n"
+		
+		assert(file.open(file_name,File.WRITE) == OK)
+		file.store_string(data_to_save)
 
 
 func _ready() -> void:
