a48ce4dc9bec5078e8c2e0394cbea178b4d20b07
Rafał Mikrut <mikrutrafal@protonmail.com>
2023-11-25 08:40:22 +0100

    Restore

--- Autoloads/BasicData.gd
+++ Autoloads/BasicData.gd
@@ -20,6 +20,7 @@ var function_exceptions: Array = [
 	###
 	### Godot 4.0
 	###
+	"_restore", # 85340
 	"convert", # 84750
 	"compress_from_channels", # 84743
 	"save_webp_to_buffer", # 84743
---
+++ IssueTesting/85340.gd
@@ -0,0 +1,5 @@
+extends Node
+func _process(delta):
+	var temp_variable962 = AnimationPlayer.new()
+	add_child(temp_variable962)
+	temp_variable962._restore(null)
